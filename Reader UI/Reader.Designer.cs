a9c71fed4aca093fa5d45ea44a5118e791086534
Cyberboss <cyberboss1994@hotmail.com>
2015-07-01 09:15:15 -0400

    Added Icon to reader

--- Reader UI/Reader.Designer.cs
+++ Reader UI/Reader.Designer.cs
@@ -264,7 +264,6 @@
             this.Controls.Add(this.jumpButton);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.openArchiver);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Reader";
             this.Text = "MS Paint Adventures";
